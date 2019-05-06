244b07a50ff54b55a1549dd8389da035e892d290
jakepucan <jakepucan@gmail.com>
2019-05-06 14:52:30 +0800

    Changes

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ljfreelancer88\blogger;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
